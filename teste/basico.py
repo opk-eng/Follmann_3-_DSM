e2e394940e1eb46f82401c0447c7184b39e143f0
Seu Nome <seuemail@exemplo.com>
2026-05-20 11:47:00 -0300

    revisao

---
+++ teste/basico.py
@@ -0,0 +1,28 @@
+# imprimir uma mensagem 
+print("Hello World!")
+
+# variável
+# snake case = nome_aluno
+# camel case = nomeAluno
+nome_aluno = "João"
+idade_aluno = 18
+
+# input = entrada de informações
+# output + sída de informações
+print("saída do python para o terminal")
+nome = input("digite seu nome: ")
+print("olá,", nome)
+# tipos de dados
+# string = texto
+# integer = número inteiro
+# float = número decimal
+# boolean = verdadeiro ou falso
+# print recebe operações 
+num1 = 10
+num2 = 15 
+print(int(num1) + int(num2)) # soma
+print(int(num1) - int(num2)) # subtração
+print(int(num1) * int(num2)) # multiplicação
+print(int(num1) / int(num2)) # divisão
+print(int(num1) ** int(num2)) # potência
+print(int(num1) % int(num2)) # resto da divisão
